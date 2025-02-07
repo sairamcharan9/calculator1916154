459fa3990aabfe43e77a5038aa5892a857d3a6f3
sairamcharan9 <sb2853@njit.edu>
2025-02-06 20:07:36 -0500

    added subtraction function

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,3 +1,7 @@
 def add(a,b):
     '''Addition fucntion'''
     return a + b
+
+def subtraction(a,b):
+    '''Subtraction function'''
+    return a - b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,10 @@
 '''Testing Calculator'''
-from calculator import add
+from calculator import add,subtraction
 
 def test_addition():
     '''Testing addition function'''    
     assert add(2,2) == 4
+
+def test_subtraction():
+    '''Testing addition function'''
+    assert subtraction(3,1) == 2
