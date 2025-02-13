2e8711702ea3bb7864f1e07b103725cc287f3965
sairamcharan9 <sb2853@njit.edu>
2025-02-13 01:04:07 -0500

    bug fixes

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,18 @@
-'''Testing Calculator'''
-from calculator import add,subtraction
+'''My Calculator Test'''
+from calculator import Calculator
 
 def test_addition():
-    '''Testing addition function'''    
-    assert add(2,2) == 4
+    '''Test that addition function works '''    
+    assert Calculator.add(2,2) == 4
 
 def test_subtraction():
-    '''Testing addition function'''
-    assert subtraction(3,1) == 2
+    '''Test that addition function works '''    
+    assert Calculator.subtract(2,2) == 0
+
+def test_divide():
+    '''Test that division function works '''    
+    assert Calculator.divide(2,2) == 1
+
+def test_multiply():
+    '''Test that multiply function works '''    
+    assert Calculator.multiply(2,2) == 4
